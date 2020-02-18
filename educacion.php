04cd8944cef2023a61ea95b84528d8b1fcf1a230
Ricardo8421 <61171940+Ricardo8421@users.noreply.github.com>
2020-02-18 02:59:54 -0600

    Add files via upload

---
+++ educacion.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Educación</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+	<div class="main-container">
+		<nav>
+			<h1>Realidad virtual</h1>
+			<h2>
+				<a href="index.php">Inicio</a>&nbsp&nbsp&nbsp&nbsp&nbsp
+				<a href="entretenimiento.php">Entretenimiento</a>&nbsp&nbsp&nbsp&nbsp&nbsp
+				<a href="medicina.php">Medicina</a>&nbsp&nbsp&nbsp&nbsp&nbsp
+				<a href="educacion.php">Educación</a>&nbsp&nbsp&nbsp&nbsp&nbsp
+				<a href="plataforma.php">¿Dónde lo consigo?</a>
+			</h2>
+		</nav>
+		<section id="nel">
+			<article>
+				<h3>
+					<p>Hace algunas pocas décadas, la realidad virtual era solo ficción. ¿Cómo podríamos imaginarnos que un día esta y otras tecnologías serían parte del cotidiano?</p>
+					<p>Si bien en la actualidad la realidad virtual no hace parte del día a día de todos, sabemos de su existencia y de la posibilidad de utilizarla a nuestro favor.</p>
+					<p>La educación es una de las áreas que menos se ha desarrollado en estas décadas. La manera de enseñar no ha evolucionado tanto como la tecnología lo ha hecho. Sin embargo, esto es algo que puede, y debe, cambiar.</p>
+					<p>¿Cómo se puede aplicar la realidad virtual a la educación?</p>
+					<p>A través de la tecnología los niños se transportan, y a pesar de lo que muchos dicen, desarrollan su imaginación, creatividad y aprenden. Se divierten en un ambiente innovador y descubren muchas posibilidades.</p>
+					<p>¿Acaso los niños no aprenden rápidamente la dinámica de los videojuegos? Desarrollan estrategias sin saber que lo están haciendo y pueden pasar mucho tiempo entretenidos de esta forma.</p>
+					<p>¿Cómo entonces la educación tradicional puede competir con la tecnología? Parece una competencia desleal. Por un lado, un mundo lleno de posibilidades y por el otro, algo monótono.</p>
+					<p>Sin embargo, la tecnología y la educación no necesitan estar de lados opuestos. Pueden trabajar juntos, con el mismo objetivo y cuando sea así, ¿te imaginas todo lo que se puede ganar?</p>
+					<p>Algunos ejemplos son:</p>
+					<li>Viajar y explorar sin limites</li>
+					<li>Viajar en el tiempo</li>
+					<li>Explorar el cuerpo humano</li>
+					<li>Generar empatía e involucrar valores</li>
+				</h3>
+			</article>
+			<figure>
+				<img src="img/rve.jpg">
+			</figure>
+		</section>
+			<aside>
+				<h3>
+					<p>Ejemplos de algunas aplicaciones</p>
+					<li>VR Lessons by ThingLink</li>
+					<p>Esta aplicación trae clases interactivas de ciencia, lenguaje y arte.</p>
+					<li>Unimersiv</li>
+					<p>Esta es una de las plataformas más grandes de experiencias educativas en realidad virtual. Con sus aplicaciones se puede aprender sobre el espacio, anatomía e historia.</p>
+					<li>VR Language App by Mondly</li>
+					<p>También se puede aprender idiomas con la ayuda de la realidad virtual. La aplicación de Mondly enseña diálogos básicos en más de 30 lenguas.</p>
+					<li>Anatomyou VR</li>
+					<p>Esta aplicación para aprender anatomía humana se encuentra disponible también en español.</p>
+				</h3>
+			</aside>
+		<footer><h4>Hecho por Gómez Tagle Espinosa José Ricardo</h4></footer>
+	</div>
+</body>
+</html>
