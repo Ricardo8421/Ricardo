16729d5a0101372e3d5c43633919ea48421412cb
Ricardo8421 <61171940+Ricardo8421@users.noreply.github.com>
2020-02-18 02:42:40 -0600

    Add files via upload

---
+++ plataforma.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>¿Dónde lo consigo?</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+	<div class="main-container">
+		<nav>
+			<h1>¿Dónde consigo realidad virtual?</h1>
+			<h2>
+				<a href="index.html">Inicio</a>&nbsp&nbsp&nbsp&nbsp&nbsp
+				<a href="entretenimiento.html">Entretenimiento</a>&nbsp&nbsp&nbsp&nbsp&nbsp
+				<a href="medicina.html">Medicina</a>&nbsp&nbsp&nbsp&nbsp&nbsp
+				<a href="educacion.html">Educación</a>&nbsp&nbsp&nbsp&nbsp&nbsp
+				<a href="plataforma.html">¿Dónde lo consigo?</a>
+			</h2>
+		</nav>
+		<section id="simon">
+			<article>
+				<h3>
+					<p>La realidad virtual ha comenzado a despegar; hace algunos años era prácticamente nulo el campo de desarrollo de esta tecnología, pero ahora cada vez más empresas han comenzado a interesarse y presentar desarrollos verdaderamente interesantes, ejemplos claros son Oculus Rift y HTC Vive Pre, por mencionar a algunos.</p>
+					<p>En el caso de nuestro país, hay algunos desarrollos que se han hecho camino al mercado nacional, pero lamentablemente aún no existe una variedad muy grande. Para quienes se han interesado en este tema, hemos hecho una recopilación de las maneras más sencillas de hacerse con algún visor de realidad virtual desde México.</p>
+					<li><a href="https://arvr.google.com/cardboard/">Google Cardboard</a></li>
+					<p>Imposible iniciar el listado con otro visor que no sea el Cardboard de Google, considerado como la apuesta más económica de la realidad virtual actual debido a que se trata básicamente de cartón, lentes, imanes y otros pocos componentes de uso diario.</p>
+					<p>De hecho, en el sitio oficial de Google están disponibles las instrucciones para construir el propio y personalizarlo al antojo de cada usuario. Lamentablemente, a pesar de que también se ofertan algunas opciones, tanto del buscador como de otros fabricantes, no es posible comprar desde México, ya sea debido a que la Google Store no tiene presencia nacional o porque los fabricantes no hacen envío hasta aquí.</p>
+					<li><a href="https://www.samsung.com/es/wearables/gear-vr-sm-r325nzvaphe/">Samsung Gear VR</a></li>
+					<p>El Gear VR, apuesta de Samsung presentada en 2014, tiene en México poco más de un año, tiempo en el que poco a poco se ha posicionado como una de las apuestas de mayor expansión en el mercado, impulsado por supuesto por la fama que el fabricante tiene en suelo nacional.</p>
+					<p>Es realmente sencillo hacerse con este VR headset pues tiendas como Best Buy y Liverpool lo comercializan por 1,999 pesos, precio al que originalmente fue lanzado.</p>
+					<li><a href="http://www.vr-box.es/">Tera VR Box</a></li>
+					<p>Otra apuesta que se puede encontrar en Amazon México a precios variables dependiendo del modelo y los aditamentos que se necesiten. Por ejemplo, la versión 2 del visor junto con un pequeño control Bluetooth, modelo de la imagen, puede comprarse por 999 pesos, aunque también hay otras versiones más económicas. Todo depende de las necesidades del usuario.</p>
+					<li><a href="https://www.vive.com/mx/">HTC Vive</a></li>
+					<p>Ahora sí, uno de los pesos pesados de la realidad virtual, el HTC Vive. Este visor, a diferencia de los anteriores, es un sistema completo que no necesita de un smartphone para el procesamiento y reproducción de contenido sino que se conecta directamente a una computadora para ello. Además cuenta con altavoces integrados y controles que complementan la experiencia.</p>
+					<p>A pesar de que ya puede comprar oficialmente en varios países por un precio aproximado de 799 dólares, en México esto no es posible. Aún así, se puede adquirir en Mercado Libre por el exorbitante precio de 27,069 pesos.</p>
+					<li><a href="https://www.oculus.com/?locale=es_ES">Oculus Rift</a></li>
+					<p>El Oculus Rift es el dispositivo que podríamos considerar como el pionero de esta rama pues a pesar de que apenas en este año se hizo oficial su comercialización, su desarrollo lleva ya unos cuantos años, hasta que fue comprada por Facebook y se impulsó aún más el énfasis en esta tecnología.</p>
+					<p>El Oculus tiene un precio aproximado de 599 dólares en los países en donde se puede adquirir de manera oficial, pero al igual que el Vive de HTC, no se puede comprar desde México. De nuevo recurrimos a otras alternativas y en esta ocasión es Amazon la tienda en donde lo podemos adquirir por 19,999 pesos. El paquete incluye visor, los cables de conexión, control, control de Xbox One, y dos juegos en realidad virtual: EVE: Valkyrie y Lucky's Tale.</p>
+				</h3>
+			</article>
+		</section>
+		<footer><h4>Hecho por Gómez Tagle Espinosa José Ricardo</h4></footer>
+	</div>
+</body>
+</html>
